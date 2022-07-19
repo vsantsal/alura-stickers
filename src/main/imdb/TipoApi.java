27948095b88eeb875545d8407589a972f3074c3c
vsantsal <vsantsal@gmail.com>
2022-07-18 22:42:34 -0300

    iniciando projeto java e enum de apis imdb

---
+++ src/main/imdb/TipoApi.java
@@ -0,0 +1,15 @@
+package main.imdb;
+
+public enum TipoApi {
+
+    TOP_250 {
+        @Override
+        public String getUrl(String chave) {
+            return "https://imdb-api.com/en/API/Top250Movies/" + chave;
+        }
+
+    };
+
+    public abstract String getUrl(String chave);
+
+}
---
+++ src/tests/imdb/TipoApiTest.java
@@ -0,0 +1,19 @@
+package tests.imdb;
+
+import main.imdb.TipoApi;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TipoApiTest {
+
+    @Test
+    public void testTipoApiDeveRetornarUrlComChavePassada(){
+        TipoApi tipoApi = TipoApi.TOP_250;
+        String chaveTeste = "chave_teste";
+        String urlObtida = tipoApi.getUrl(chaveTeste);
+        assertEquals("https://imdb-api.com/en/API/Top250Movies/" + chaveTeste,
+                urlObtida);
+
+    }
+
+}
